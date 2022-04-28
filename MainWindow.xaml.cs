9b9a853b09aece40b423859847142aef3d42fd5a
Saha <t343st@gmail.com>
2022-04-28 23:01:00 +0300

    Начинаю делать темы

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using a =Microsoft.Windows.Themes;
+
 
 namespace WPF_Cartridge
 {
@@ -25,5 +28,43 @@ namespace WPF_Cartridge
             InitializeComponent();
             Fmain.Navigate(new PageFolder.TablePage());
         }
+
+
+        #region Events
+        private void BBlackTheme_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeTheme('d');
+        }
+
+        private void BWhiteTheme_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeTheme('w');
+        }
+        #endregion
+
+        #region Methods
+        private void ChangeTheme(char theme)
+        {
+            string appPath = "App.xaml";
+            string pathTheme = "";
+            if (theme == 'd')
+            {
+                pathTheme = "ThemeFolder\\DarkTheme.xaml";
+            }
+            else
+            {
+                pathTheme = "ThemeFolder\\LightTheme.xaml";
+            }
+            var uriTheme = new Uri(pathTheme, UriKind.Relative);
+            var uriApp = new Uri(appPath, UriKind.Relative);
+            Application.Current.Resources.Clear();
+            ResourceDictionary resourceDictTheme = Application.LoadComponent(uriTheme) as ResourceDictionary;
+            //ResourceDictionary resourceDictApp = Application.LoadComponent(uriApp) as ResourceDictionary;
+            Application.Current.Resources.MergedDictionaries.Add(resourceDictTheme);
+            //Application.Current.Resources.MergedDictionaries.Add(resourceDictApp);
+        }
+        #endregion
+
+
     }
 }
